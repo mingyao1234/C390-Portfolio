81df58ec6880fe716cdd6e84c710fa9afe9e3b0b
Quah Ming Yao <21023135@myrp.edu.sg>
2023-04-26 14:55:51 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+anime({
+    targets: '#quartercircle',
+    translateY: [-300, 0],
+    translateX: [-200, 0],
+    delay: 200,
+    duration: 800,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets:'#mainlogo',
+    translateX: [-100, 0],
+    opacity: [0, 1],
+    delay: 700,
+    duration: 700,
+    easing: 'easeInOutExpo'
+})
+
+anime({
+    targets: '#typingcursor',
+    translateX: [0, 440],
+    delay: 450,
+    duration: 200,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#typingcursor',
+    opacity: [1, 0, 1],
+    delay: 300,
+    duration: 1100,
+    loop: true
+});
+
+anime({
+    targets: '.headertext',
+    opacity: [0, 1],
+    delay: 650,
+    duration: 1000
+});
+
+anime({
+    targets: '.subtext',
+    opacity: [0, 1],
+    translateX: [-100, 0],
+    delay: 1000,
+    duration: 1000,
+    easing: 'easeInOutExpo'
+})
+
+anime({
+    targets: '#homebtns',
+    translateY: [50, 0],
+    opacity: [0, 1],
+    delay: 1500,
+    duration: 700,
+    easing: 'easeInOutExpo'
+})
+
+anime({
+    targets: '#abouttext',
+    translateX: [70, 0],
+    opacity: [0, 1],
+    delay: 200,
+    duration: 800,
+    easing: 'easeInOutExpo'
+});
+
+anime({
+    targets: '#aboutimage',
+    translateX: [-200, 0],
+    opacity: [0, 1],
+    delay: 200,
+    duration: 800,
+    easing: 'easeInOutExpo'
+});
